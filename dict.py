1264664037d4e2b3c0a450e5bc226c86f02a03e5
Abhi37231 <159634392+Abhi37231@users.noreply.github.com>
2025-08-07 15:44:27 +0530

    07/08

---
+++ dict.py
@@ -0,0 +1,18 @@
+dict={"name":"Abhinandan","rollno":65,"branch":"CSE"}
+
+print("Name:",dict["name"])
+print("Roll No:",dict["rollno"])
+print("Branch:",dict["branch"])
+
+print(dict.keys())
+
+print(dict.values())
+
+print(dict.items())
+
+print(dict.get("rollno"))
+
+dict.update({"Div":"A"})
+print(dict)
+
+
---
+++ tupel.py
@@ -0,0 +1,23 @@
+tup =(2,3,4,5,4)
+print(tup)
+print(type(tup))
+
+print(tup.index(4))
+
+print(tup.count(4))
+
+
+
+
+my_tuple = ("apple", "banana", "cherry")
+
+
+print(my_tuple[0]) 
+
+mixed_tuple = (1, "hello", 3.14)
+
+
+a, b, c = my_tuple
+print(a)  
+print(b)  
+print(c)  
