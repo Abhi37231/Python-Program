d6201650839e29d83755f4241b62cece47d26870
Abhi37231 <159634392+Abhi37231@users.noreply.github.com>
2025-09-04 15:23:45 +0530

    04/09

---
+++ directoryHandling/p1.py
@@ -0,0 +1,10 @@
+import os
+
+# os.mkdir("Abhinandan")
+
+
+print("String format :", os.getcwd())
+
+print("Byte string format :", os.getcwdb())
+
+os.rename("Abhinandan", "renamed_directory")
---
+++ package/main.py
@@ -0,0 +1,7 @@
+from my_package import math,string
+
+print("Addition",math.add(2,4))
+print("Substract",math.sub(5,2))
+
+print("Capitalized:",string.capitalize_words("Hi! My Self Abhinandan"))
+print("Reverse:",string.reverse_string(" esc ni gniduts ma I"))
